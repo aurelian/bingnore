d6f276fa43aedb992d954fdd15a478525f7293a2
Aurelian Oancea <oancea@gmail.com>
2020-08-21 15:06:37 +0200

    August 2020

--- install.fish
+++ install.fish
@@ -3,8 +3,9 @@
 brew install fzf
 brew install ack
 
-ln -s (pwd)/fish/config.fish ~/.config/fish/config.fish
-ln -s (pwd)/dotfiles/gemrc ~/.gemrc
-ln -s (pwd)/dotfiles/gitconfig ~/.gitconfig
-ln -s (pwd)/fish/functions ~/.config/fish/functions
-
+ln -sf (pwd)/fish/config.fish ~/.config/fish/config.fish
+ln -sf (pwd)/dotfiles/gemrc ~/.gemrc
+ln -sf (pwd)/dotfiles/gitconfig ~/.gitconfig
+ln -sf (pwd)/fish/functions ~/.config/fish/functions
+ln -sf (pwd)/dotfiles/tmux.conf ~/.tmux.conf
+ln -sf (pwd)/dotfiles/ackrc ~/.ackrc
